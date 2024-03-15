0ce027ec03869b6fc778ebf768eceea94100c00a
Riuseigi <troyliamjarata@gmail.com>
2024-03-15 11:11:54 +0800

    make the visualizer use in background

--- scripts/script.js
+++ scripts/script.js
@@ -61,7 +61,7 @@ function draw() {
 
   analyser.getByteFrequencyData(dataArray);
 
-  ctx.fillStyle = 'rgb(0, 0, 0)';
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.1)'; // Use rgba to set the fill style with alpha channel
   ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
   const barWidth = (WIDTH / bufferLength) * 2.5;
@@ -70,7 +70,7 @@ function draw() {
   for (let i = 0; i < bufferLength; i++) {
     const barHeight = dataArray[i] * 2;
 
-    ctx.fillStyle = `rgb(${barHeight + 100}, 50, 50)`;
+    ctx.fillStyle = `rgba(${barHeight + 100}, 5, 50, 1)`; // Adjust alpha value as needed
     ctx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight);
 
     x += barWidth + 1;
@@ -94,6 +94,7 @@ audio.addEventListener('ended', () => {
 });
 
 draw();
+
 
 
 
