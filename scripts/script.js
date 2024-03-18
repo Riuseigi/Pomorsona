f60879cf381eea3525d62caeba0c3dee6ef96ad2
Riuseigi <troyliamjarata@gmail.com>
2024-03-18 14:40:50 +0800

    add button sound when clicked

--- scripts/script.js
+++ scripts/script.js
@@ -68,6 +68,27 @@ updateTimer();
 
 
 
+const buttons = document.querySelectorAll('button')
+/**
+ * Plays a click sound.
+ *
+ * @param {none} - No parameters
+ * @return {void} No return value
+ */
+function playClickSound() {
+  const audio = new Audio('select.mp3'); // Path to your click sound file
+  audio.play();
+  audio.volume = 0.1;
+}
+
+// Add click event listener to all buttons
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    playClickSound();
+  });
+});
+
+
 
 
 
