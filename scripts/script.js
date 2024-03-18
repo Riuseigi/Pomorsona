4709c84a14c4c3d745303024755bda5c484a178b
Riuseigi <troyliamjarata@gmail.com>
2024-03-18 13:59:44 +0800

    add breaktime function and changeable element

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,56 @@
-let time = 1 * 60; // 25 minutes in seconds
+let workTime = 1 * 60; // 25 minutes in seconds
+let breakTime = 5 * 60; // 5 minutes in seconds
+let isWorking = true; // Indicates if it's currently work time
 let intervalId;
-
+const timerTitle = document.querySelector('.timer__title');
 function updateTimer() {
+  let time;
+  if (isWorking) {
+    timerTitle.textContent = 'Focus';
+    time = workTime;
+  } else {
+    timerTitle.textContent = 'Breaktime';
+    time = breakTime;
+  }
+
   const minutes = Math.floor(time / 60).toString().padStart(2, '0');
   const seconds = (time % 60).toString().padStart(2, '0');
   document.querySelector('.timer__minutes').textContent = `${minutes}:${seconds}`;
 }
 
-document.querySelector('.start').addEventListener('click', () => {
+/**
+ * Starts a timer for work and break intervals, updating the timer display and resetting the timer as needed.
+ *
+ * @param None
+ * @return None
+ */
+function startTimer() {
   intervalId = setInterval(() => {
-    if (time > 0) {
-      time--;
-      updateTimer();
+    if (isWorking) {
+      workTime--;
     } else {
+      breakTime--;
+    }
+
+    updateTimer();
+
+    if (workTime === 0 && isWorking) {
       clearInterval(intervalId);
-      // Play sound or show visual cue
+      isWorking = false;
+      updateTimer();
+      startTimer();
+    } else if (breakTime === 0 && !isWorking) {
+      clearInterval(intervalId);
+      isWorking = true;
+      workTime = 25 * 60;
+      breakTime = 5 * 60;
+      updateTimer();
+      startTimer();
     }
   }, 1000);
-});
+}
+
+document.querySelector('.start').addEventListener('click', startTimer);
 
 document.querySelector('.pause').addEventListener('click', () => {
   clearInterval(intervalId);
@@ -25,7 +58,9 @@ document.querySelector('.pause').addEventListener('click', () => {
 
 document.querySelector('.reset').addEventListener('click', () => {
   clearInterval(intervalId);
-  time = 25 * 60;
+  isWorking = true;
+  workTime = 25 * 60;
+  breakTime = 5 * 60;
   updateTimer();
 });
 
