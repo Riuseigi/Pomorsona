f41091b3c3d42be9978388e0426e532257dfe106
Riuseigi <troyliamjarata@gmail.com>
2024-03-20 22:37:08 +0800

    add modal element and style for settings of pomodoro time

--- scripts/script.js
+++ scripts/script.js
@@ -89,6 +89,49 @@ buttons.forEach(button => {
 });
 
 
+//modal content
+
+// Get the modal element
+var modal = document.getElementById('settingsModal');
+
+// Get the button that opens the modal
+var settingsBtn = document.getElementById('settingsBtn');
+
+// Get the <span> element that closes the modal
+var closeBtn = document.getElementsByClassName('close')[0];
+
+// When the user clicks the button, open the modal
+settingsBtn.onclick = function() {
+  modal.style.display = 'block';
+};
+
+// When the user clicks on <span> (x), close the modal
+closeBtn.onclick = function() {
+  modal.style.display = 'none';
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
+
+// Save settings
+var saveBtn = document.getElementById('saveSettings');
+saveBtn.onclick = function() {
+  var workDuration = document.getElementById('workDuration').value;
+  var breakDuration = document.getElementById('breakDuration').value;
+
+  // Save the settings to localStorage or wherever you store them
+  // For example:
+  localStorage.setItem('workDuration', workDuration);
+  localStorage.setItem('breakDuration', breakDuration);
+
+  // Close the modal
+  modal.style.display = 'none';
+};
+
 
 
 
